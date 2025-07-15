11ce8716168940f9d55e7875d70ca9cacf8b03bf
Nijesu-alt <testimonyobanijesumi@gmail.com>
2025-07-15 16:29:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning App')
 
 st.info('This app builds a machine learning model')
+
+df = pd.read_csv('customer_churn_dataset_testing_master')
+df
